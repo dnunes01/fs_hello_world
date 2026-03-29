9db7c6b158f3c6ad9f6c1a00bdfba531385ebda1
Dan Nunes <dnunes.utah@gmail.com>
2026-03-29 00:27:53 -0600

    Spring setup complete, and HelloController working. Confirmed access from cell device on same network

---
+++ hello-world-backend/src/main/java/com/dan/helloworld/HelloWorldBackendApplication.java
@@ -0,0 +1,11 @@
+package com.dan.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldBackendApplication.class, args);
+    }
+}
