adfc1d7c94e6171e2228bf3365ba08702007f180
Dan Nunes <dnunes.utah@gmail.com>
2026-05-09 23:05:42 -0600

    feat(users): add signal-driven user state, replace NgFor with @for, and connect UI to backend API

--- hello-world-frontend/src/app/app.ts
+++ hello-world-frontend/src/app/app.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
-  protected readonly title = signal('hello-world-frontend');
+
+  users = signal<any[]>([]);
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe(users => {
-      console.log('Users from backend: ', users);
-    })
+    this.userService.getUsers().subscribe({
+      next: users => this.users.set(users),
+      error: err => console.error('Error loading users:', err)
+    });
   }
-
 }
